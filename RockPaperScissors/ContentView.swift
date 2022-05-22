b70c26e0dcefa4417e720d330c3c869630aed94d
Miguel Quezada <mig.aquezada@gmail.com>
2022-05-21 23:30:07 -0400

    fix counter for score to not go below 0

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,9 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    let moves = ["✊", "✋", "✌️"]
+
+    @State private var computerChoice = Int.random(in: 0..<3)
+    @State private var shouldWin = Bool.random()
+
+    @State private var score = 0
+    @State private var questionCount = 1
+    @State private var showingResults = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+
+            Text("Computer has played…")
+                .font(.headline)
+
+            Text(moves[computerChoice])
+                .font(.system(size: 200))
+
+            if shouldWin {
+                Text("Which one wins?")
+                    .foregroundColor(.green)
+                    .font(.title)
+            } else {
+                Text("Which one loses?")
+                    .foregroundColor(.red)
+                    .font(.title)
+            }
+
+            HStack {
+                ForEach(0..<3) { number in
+                    Button(moves[number]) {
+                        play(choice: number)
+                    }
+                    .font(.system(size: 80))
+                }
+            }
+
+            Spacer()
+
+            Text("Score: \(score)")
+                .font(.subheadline)
+
+            Spacer()
+        }
+        .alert("Game over", isPresented: $showingResults) {
+            Button("Play Again", action: reset)
+        } message: {
+            Text("Your score was \(score)")
+        }
+    }
+
+    func play(choice: Int) {
+        let winningMoves = [1, 2, 0]
+        let didWin: Bool
+
+        if shouldWin {
+            didWin = choice == winningMoves[computerChoice]
+        } else {
+            didWin = winningMoves[choice] == computerChoice
+        }
+
+        if didWin {
+            score += 1
+        } else {
+            
+            if score > 0{
+                score -= 1}
+        }
+
+        if questionCount == 10 {
+            showingResults = true
+        } else {
+            computerChoice = Int.random(in: 0..<3)
+            shouldWin.toggle()
+            questionCount += 1
+        }
+    }
+
+    func reset() {
+        computerChoice = Int.random(in: 0..<3)
+        shouldWin = Bool.random()
+        questionCount = 0
+        score = 0
     }
 }
 
